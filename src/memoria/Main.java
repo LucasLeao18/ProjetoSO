41adbcc93cedf39ef98322456130dcbd13f0bdb2
Guilherme Magalhães <guiric34@gmail.com>
2025-07-02 12:16:59 -0300

    Implementação da FabricaDeEntradas e integração do mesmo com o sistema

--- src/memoria/Main.java
+++ src/memoria/Main.java
@@ -2,6 +2,7 @@ package memoria;
 
 import memoria.algoritmo.AlgoritmoNRU;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * Ponto de entrada: inicializa o console e dispara os processos.
@@ -13,16 +14,18 @@ public class Main {
         GerenciadorDeMemoria ger = new GerenciadorDeMemoria(
             3, 10, new AlgoritmoNRU()
         );
+        String entrada = new FabricaDeEntradas(10).getNewEntrada();
+        List<String> operacoes = Arrays.asList(entrada.split(","));
 
-        Processo p1 = new Processo(1,
-            Arrays.asList("1-R", "2-R", "3-W-99", "1-W-5", "4-R", "5-R"),
-            ger);
+        Processo p1 = new Processo(1, operacoes, ger);
+        Processo p2 = new Processo(2, operacoes, ger);
+        Processo p3 = new Processo(3, operacoes, ger);
+        Processo p4 = new Processo(4, operacoes, ger);
 
-        Processo p2 = new Processo(2,
-            Arrays.asList("6-R", "2-R", "1-R", "3-W-8", "7-R"),
-            ger);
 
         p1.start();
         p2.start();
+        p3.start();
+        p4.start();
     }
 }
